7bd4b0958b933a62ac029ee03d49990783cfebe6
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-09 19:56:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -28,7 +28,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    self.all.detect {|artist| artist.name == name} || Artist.new(na).save
+    self.all.detect {|artist| artist.name == name} || Artist.new(name).save
   end
 
   def print_songs
